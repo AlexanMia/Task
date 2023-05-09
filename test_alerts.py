e7befd4b58f6f205c3419e6ef40ea987e2431bdc
Inna <idinna91@mail.ru>
2023-05-09 12:05:39 +0300

    update tests alerts

--- test_alerts.py
+++ test_alerts.py
@@ -1,4 +1,5 @@
 import pytest
+from assertpy import soft_assertions, assert_that
 from test_base import TestBase
 from util.constants import Constants
 
@@ -16,33 +17,51 @@ class TestEndToEndAlerts(TestBase):
         alerts_page.waiting_alert()
         alerts_page.switch_to_alert()
 
-        assert alerts_page.get_text_from_alert() == Constants.INFO_FROM_ALERT_TIMER, \
-            f'{alerts_page.get_text_from_alert()} != {Constants.INFO_FROM_ALERT_TIMER}'
+        with soft_assertions():
+            assert_that(alerts_page.get_text_from_alert()) \
+                .described_as(f'Check alert_timer text is equal to {Constants.INFO_FROM_ALERT_TIMER}') \
+                .is_equal_to(Constants.INFO_FROM_ALERT_TIMER)
 
         alerts_page.accept_alert()
 
     def test_open_alert_confirm(self):
         alerts_page.click_button_open_alert_confirm()
         alerts_page.switch_to_alert()
-        assert alerts_page.get_text_from_alert() == Constants.INFO_FROM_CONFIRM_ALERT, \
-            f'{alerts_page.get_text_from_alert()} != {Constants.INFO_FROM_CONFIRM_ALERT}'
+
+        with soft_assertions():
+            assert_that(alerts_page.get_text_from_alert()) \
+                .described_as(f'Check alert_confirm text is equal to {Constants.INFO_FROM_CONFIRM_ALERT}') \
+                .is_equal_to(Constants.INFO_FROM_CONFIRM_ALERT)
 
         alerts_page.accept_alert()
 
-        assert alerts_page.find_results_confirm_field(), 'There is not field with results'
-        assert alerts_page.text_from_result_confirm_field() == Constants.INFO_FROM_ALERT_ACCEPT, \
-            f'{alerts_page.text_from_result_confirm_field()} != {Constants.INFO_FROM_ALERT_ACCEPT}'
+        with soft_assertions():
+            assert_that(alerts_page.find_results_confirm_field()) \
+                .described_as('There is the field with results') \
+                .is_true()
+
+            assert_that(alerts_page.text_from_result_confirm_field()) \
+                .described_as(f'Check result of alert_confirm text is equal to {Constants.INFO_FROM_ALERT_ACCEPT}') \
+                .is_equal_to(Constants.INFO_FROM_ALERT_ACCEPT)
 
     def test_open_alert_prompt(self):
         alerts_page.click_button_open_alert_prompt()
         alerts_page.switch_to_alert()
 
-        assert alerts_page.get_text_from_alert() == Constants.INFO_FROM_PROMPT_ALERT, \
-            f'{alerts_page.get_text_from_alert()} != {Constants.INFO_FROM_PROMPT_ALERT}'
+        with soft_assertions():
+            assert_that(alerts_page.get_text_from_alert()) \
+                .described_as(f'Check alert_prompt text is equal to {Constants.INFO_FROM_PROMPT_ALERT}') \
+                .is_equal_to(Constants.INFO_FROM_PROMPT_ALERT)
 
         alerts_page.post_text_to_alert(Constants.NAME_FOR_ALERT)
         alerts_page.accept_alert()
 
-        assert alerts_page.find_results_prompt_field(), 'There is not field with results'
-        assert alerts_page.text_from_result_prompt_field() == Constants.INFO_FROM_PROMPT_ALERT_ACCEPT, \
-            f'{alerts_page.text_from_result_prompt_field()} != {Constants.INFO_FROM_PROMPT_ALERT_ACCEPT}'
+        with soft_assertions():
+            assert_that(alerts_page.find_results_prompt_field()) \
+                .described_as('There is the field with results') \
+                .is_true()
+
+            assert_that(alerts_page.text_from_result_prompt_field()) \
+                .described_as(
+                f'Check result of alert_confirm text is equal to {Constants.INFO_FROM_PROMPT_ALERT_ACCEPT}') \
+                .is_equal_to(Constants.INFO_FROM_PROMPT_ALERT_ACCEPT)
